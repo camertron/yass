167ed6bac1de707bb6fa294254dc1c32e706a3c3
Cameron Dutro <camertron@gmail.com>
2026-03-30 13:31:44 -0700

    Add support for the block size declaration

--- ext/yass/src/declarations/block_size.rs
+++ ext/yass/src/declarations/block_size.rs
@@ -1,12 +1,30 @@
+use magnus::{DataTypeFunctions, Ruby, TypedData, Value, gc, typed_data};
 use style::values::specified::Size;
 
-#[magnus::wrap(class = "Yass::Declarations::BlockSize")]
+use crate::{cached_value::CachedValue, declarations::size::make_size};
+
+#[derive(TypedData)]
+#[magnus(class = "Yass::Declarations::BlockSize", mark)]
 pub struct YBlockSize {
-  size: Size
+    size: CachedValue<Size>
 }
 
 impl YBlockSize {
-  pub fn new(size: Size) -> Self {
-    Self { size }
-  }
+    pub fn new(size: Size) -> Self {
+        Self {
+            size: CachedValue::new(size, |s, ruby| {
+                make_size(s.clone(), ruby)
+            })
+        }
+    }
+
+    pub fn size(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Value {
+        rb_self.size.get(ruby)
+    }
+}
+
+impl DataTypeFunctions for YBlockSize {
+    fn mark(&self, marker: &gc::Marker) {
+        self.size.mark(marker);
+    }
 }
