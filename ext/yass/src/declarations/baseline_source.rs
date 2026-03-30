89a9a3e95774c2f713cf69d630361d524d6658e7
Cameron Dutro <camertron@gmail.com>
2026-03-30 12:01:10 -0700

    Add support for the baseline source declaration

--- ext/yass/src/declarations/baseline_source.rs
+++ ext/yass/src/declarations/baseline_source.rs
@@ -1,12 +1,21 @@
+use magnus::{Ruby, typed_data, value::Id};
 use style::values::computed::BaselineSource;
 
 #[magnus::wrap(class = "Yass::Declarations::BaselineSource")]
 pub struct YBaselineSource {
-  baseline_source: BaselineSource
+    baseline_source: BaselineSource
 }
 
 impl YBaselineSource {
-  pub fn new(baseline_source: BaselineSource) -> Self {
-    Self { baseline_source }
-  }
+    pub fn new(baseline_source: BaselineSource) -> Self {
+        Self { baseline_source }
+    }
+
+    pub fn value(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Id {
+        match rb_self.baseline_source {
+            BaselineSource::Auto => ruby.intern("auto"),
+            BaselineSource::First => ruby.intern("first"),
+            BaselineSource::Last => ruby.intern("last"),
+        }
+    }
 }
