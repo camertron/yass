ab11e403e2b2e3a81bc78989e16f65b54b080791
Cameron Dutro <camertron@gmail.com>
2026-03-31 13:28:19 -0700

    Remove unused file

--- ext/yass/src/selector.rs
+++
@@ -1,5 +0,0 @@
-use std::cell::RefCell;
-
-use magnus::{DataTypeFunctions, Error, Integer, Module, RArray, RModule, RString, Ruby, TypedData, Value, gc, typed_data, value::{Id, InnerValue, Opaque, ReprValue}};
-use selectors::{SelectorList, attr::{AttrSelectorOperator, AttrSelectorWithOptionalNamespace, NamespaceConstraint, ParsedCaseSensitivity}, parser::{Combinator, Component, LocalName, NthOfSelectorData, NthSelectorData, NthType, RelativeSelector, RelativeSelectorMatchHint, Selector}};
-use style::selector_parser::{NonTSPseudoClass, SelectorImpl};
