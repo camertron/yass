1e653768dc23dc81da74556498832c4a009b109d
Cameron Dutro <camertron@gmail.com>
2026-03-31 15:40:06 -0700

    Add support for the caption side declaration

--- ext/yass/src/declarations/caption_side.rs
+++ ext/yass/src/declarations/caption_side.rs
@@ -1,12 +1,20 @@
+use magnus::{Ruby, typed_data, value::Id};
 use style::values::specified::table::CaptionSide;
 
 #[magnus::wrap(class = "Yass::Declarations::CaptionSide")]
 pub struct YCaptionSide {
-  caption_side: CaptionSide
+    caption_side: CaptionSide
 }
 
 impl YCaptionSide {
-  pub fn new(caption_side: CaptionSide) -> Self {
-    Self { caption_side }
-  }
+    pub fn new(caption_side: CaptionSide) -> Self {
+        Self { caption_side }
+    }
+
+    pub fn value(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Id {
+        match rb_self.caption_side {
+            CaptionSide::Top => ruby.intern("top"),
+            CaptionSide::Bottom => ruby.intern("bottom"),
+        }
+    }
 }
