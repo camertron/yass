5a69f7ec49904f389a584cd19ae7b3c2e7739a81
Cameron Dutro <camertron@gmail.com>
2026-03-30 22:17:13 -0700

    Add support for the border inline {start,end} width declarations

--- ext/yass/src/declarations/border_inline_end_width.rs
+++ ext/yass/src/declarations/border_inline_end_width.rs
@@ -1,12 +1,24 @@
+use magnus::{IntoValue, Ruby, Value, typed_data};
 use style::values::specified::BorderSideWidth;
+use style_traits::ToCss;
 
 #[magnus::wrap(class = "Yass::Declarations::BorderInlineEndWidth")]
 pub struct YBorderInlineEndWidth {
-  border_side_width: BorderSideWidth
+    border_side_width: BorderSideWidth
 }
 
 impl YBorderInlineEndWidth {
-  pub fn new(border_side_width: BorderSideWidth) -> Self {
-    Self { border_side_width }
-  }
+    pub fn new(border_side_width: BorderSideWidth) -> Self {
+        Self { border_side_width }
+    }
+
+    pub fn value(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Value {
+        // BorderSideWidth wraps LineWidth with a private field, so we have to use
+        // to_css_string instead.
+        let css = rb_self.border_side_width.to_css_string();
+        match css.as_str() {
+            keyword @ ("thin" | "medium" | "thick") => ruby.intern(keyword).into_value_with(ruby),
+            _ => ruby.str_new(&css).into_value_with(ruby),
+        }
+    }
 }
