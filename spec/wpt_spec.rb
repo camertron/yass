ea6d4a48a2503266e44fb8e3b09baa29f984c4b2
Cameron Dutro <camertron@gmail.com>
2026-04-04 08:20:06 -0700

    Add wpt tests

---
+++ spec/wpt_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "json"
+
+RSpec.describe(Yass) do
+  dirname = File.join(__dir__, "wpt", "fixtures")
+
+  Dir.chdir(dirname) do
+    Dir.glob(File.join("**", "*.css")) do |css_path|
+      it "parses #{css_path}" do
+        full_css_path = File.join(dirname, css_path)
+        sheet = Yass::Parser.parse(File.read(full_css_path))
+        expect(sheet.to_h.to_json.size).to be > 10
+      end
+    end
+  end
+end
