09c724ca32bba094c831eadf352fe3294240f6f0
Cameron Dutro <camertron@gmail.com>
2026-04-04 10:44:05 -0700

    Add support for the csswide keyword declaration

--- ext/yass/src/declarations/csswide_keyword.rs
+++ ext/yass/src/declarations/csswide_keyword.rs
@@ -1,12 +1,23 @@
-use style::properties::WideKeywordDeclaration;
+use magnus::{Ruby, typed_data, value::Id};
+use style::properties::{CSSWideKeyword, WideKeywordDeclaration};
 
 #[magnus::wrap(class = "Yass::Declarations::CSSWideKeyword")]
 pub struct YCSSWideKeyword {
-  wide_keyword_declaration: WideKeywordDeclaration
+    wide_keyword_declaration: WideKeywordDeclaration
 }
 
 impl YCSSWideKeyword {
-  pub fn new(wide_keyword_declaration: WideKeywordDeclaration) -> Self {
-    Self { wide_keyword_declaration }
-  }
+    pub fn new(wide_keyword_declaration: WideKeywordDeclaration) -> Self {
+        Self { wide_keyword_declaration }
+    }
+
+    pub fn value(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Id {
+        match rb_self.wide_keyword_declaration.keyword {
+            CSSWideKeyword::Initial => ruby.intern("initial"),
+            CSSWideKeyword::Inherit => ruby.intern("inherit"),
+            CSSWideKeyword::Unset => ruby.intern("unset"),
+            CSSWideKeyword::Revert => ruby.intern("revert"),
+            CSSWideKeyword::RevertLayer => ruby.intern("revert_layer"),
+        }
+    }
 }
