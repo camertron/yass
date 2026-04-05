45d59a2a80aff7d89ce15421993929ad79dcc155
Cameron Dutro <camertron@gmail.com>
2026-04-05 10:37:30 -0700

    Add support for the height declaration

--- ext/yass/src/declarations/height.rs
+++ ext/yass/src/declarations/height.rs
@@ -1,12 +1,30 @@
+use magnus::{DataTypeFunctions, Ruby, TypedData, Value, gc, typed_data};
 use style::values::specified::Size;
 
-#[magnus::wrap(class = "Yass::Declarations::Height")]
+use crate::{cached_value::CachedValue, declarations::size::make_size};
+
+#[derive(TypedData)]
+#[magnus(class = "Yass::Declarations::Height", mark)]
 pub struct YHeight {
-  size: Size
+    size: CachedValue<Size>
 }
 
 impl YHeight {
-  pub fn new(size: Size) -> Self {
-    Self { size }
-  }
+    pub fn new(size: Size) -> Self {
+        Self {
+            size: CachedValue::new(size, |s, ruby| {
+                make_size(s, ruby)
+            })
+        }
+    }
+
+    pub fn size(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Value {
+        rb_self.size.get(ruby)
+    }
+}
+
+impl DataTypeFunctions for YHeight {
+    fn mark(&self, marker: &gc::Marker) {
+        self.size.mark(marker);
+    }
 }
