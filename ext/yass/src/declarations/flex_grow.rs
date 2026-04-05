f8835537542a19345fd86fae9dc3631221feb658
Cameron Dutro <camertron@gmail.com>
2026-04-04 19:07:20 -0700

    Add support for the flex grow declaration

--- ext/yass/src/declarations/flex_grow.rs
+++ ext/yass/src/declarations/flex_grow.rs
@@ -1,12 +1,19 @@
+use magnus::{IntoValue, Ruby, Value, typed_data};
 use style::values::specified::NonNegativeNumber;
+
+use crate::declarations::number::YNumber;
 
 #[magnus::wrap(class = "Yass::Declarations::FlexGrow")]
 pub struct YFlexGrow {
-  non_negative_number: NonNegativeNumber
+    non_negative_number: NonNegativeNumber
 }
 
 impl YFlexGrow {
-  pub fn new(non_negative_number: NonNegativeNumber) -> Self {
-    Self { non_negative_number }
-  }
+    pub fn new(non_negative_number: NonNegativeNumber) -> Self {
+        Self { non_negative_number }
+    }
+
+    pub fn value(ruby: &Ruby, rb_self: typed_data::Obj<Self>) -> Value {
+        YNumber::new(rb_self.non_negative_number.get()).into_value_with(ruby)
+    }
 }
